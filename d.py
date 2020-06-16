4084e0a88400eae1a09747f65a85b8d7b6ba5da9
devansh <devanshtayal2005@gmail.com>
2020-06-16 18:41:40 +0530

    Commit

---
+++ d.py
@@ -0,0 +1,72 @@
+"""
+DICE
+RANDOM FACE NUMBER
+LOOPING
+
+develop random number
+check the number
+print the face 
+looping
+
+"""
+
+import random
+print("THIS IS A DICE STIMULATOR")
+
+
+
+x="y"
+while x=="y" or "Y":
+    number=random.randint(1,6)
+
+    if number==1:
+        print("--------------")
+        print("|            |")
+        print("|            |")
+        print("|    0       |")
+        print("|            |")
+        print("|            |")
+        print("--------------")
+    if number==2:
+        print("--------------")
+        print("|  0         |")
+        print("|            |")
+        print("|            |")
+        print("|            |")
+        print("|        0   |")
+        print("--------------")
+    if number==3:
+
+        print("--------------")
+        print("| 0          |")
+        print("|            |")
+        print("|    0       |")
+        print("|            |")
+        print("|         0  |")
+        print("--------------")
+    if number==4:
+        print("--------------")
+        print("|  0       0 |")
+        print("|            |")
+        print("|            |")
+        print("|            |")
+        print("|  0       0 |")
+        print("--------------")
+    if number==5:
+        print("--------------")
+        print("|  0       0 |")
+        print("|            |")
+        print("|     0      |")
+        print("|            |")
+        print("|  0       0 |")
+        print("--------------")
+
+    if number==6:
+        print("--------------")
+        print("|  0       0 |")
+        print("|            |")
+        print("|  0       0 |")
+        print("|            |")
+        print("|  0       0 |")
+        print("--------------")
+    x=input("Enter y to roll the jege")
